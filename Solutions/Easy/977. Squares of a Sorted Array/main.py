eb540204d282d20ce31bc9b9da1fd1efd46e9d84
michelebolis <mickbolis@gmail.com>
2024-03-02 09:26:43 +0100

    Solution: 977. Squares of a Sorted Array

---
+++ Solutions/Easy/977. Squares of a Sorted Array/main.py
@@ -0,0 +1,3 @@
+from solution import Solution
+print([-4,-1,0,3,10], Solution.sortedSquares(None, [-4,-1,0,3,10]))
+print([-7,-3,2,3,11], Solution.sortedSquares(None, [-7,-3,2,3,11]))
---
+++ Solutions/Easy/977. Squares of a Sorted Array/solution.py
@@ -0,0 +1,4 @@
+from typing import List
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        return sorted([x * x for x in nums])
