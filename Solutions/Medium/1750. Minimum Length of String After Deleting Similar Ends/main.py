f452ece74a2c09e824fa821a70974e16d2794ac1
michelebolis <mickbolis@gmail.com>
2024-03-05 08:55:18 +0100

    Solution: 1750. Minimum Length of String After Deleting Similar Ends

---
+++ Solutions/Medium/1750. Minimum Length of String After Deleting Similar Ends/main.py
@@ -0,0 +1,4 @@
+from solution import Solution
+print("case1: expected 2 ->", Solution.minimumLength(None, "ca"))
+print("case1: expected 0 ->", Solution.minimumLength(None, "cabaabac"))
+print("case1: expected 3 ->", Solution.minimumLength(None, "aabccabba"))
